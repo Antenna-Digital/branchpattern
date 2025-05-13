45747c120c058b1fee87e19cec1a5cb74da79639
Antenna Digital Developer <developer@antennagroup.com>
2025-05-13 05:54:43 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2346,7 +2346,7 @@ const init = () => {
     }
     
     // hide the announcement bar when the menu is open because it pushes things down too far
-    if (isOpen) {
+    if (!isOpen) {
       announcementBanner.addClass("hide-me");
     } else {
       // announcementBanner.removeClass("hide-me");
